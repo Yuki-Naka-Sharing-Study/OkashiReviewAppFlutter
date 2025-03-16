5d5efd9ad898f861e063311726c4dcb0e9dab23c
仲優樹 <yu-ki216m@outlook.jp>
2025-03-17 08:18:11 +0900

    Implemented “name of candy” and “enter text.” in review_screen.

--- lib/features/review/presentation/review_screen.dart
+++ lib/features/review/presentation/review_screen.dart
@@ -6,19 +6,36 @@ class ReviewScreen extends StatefulWidget {
 }
 
 class _ReviewScreenState extends State<ReviewScreen> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("レビュー"),
+        title: const Text("レビュー"),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text("レビュータイトル"),
-            subtitle: Text("レビュー内容"),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Text("お菓子の名前"),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'テキストを入力',
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
