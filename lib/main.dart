a392a8250e9adece0f4b61f8b3c15e074b5e65ba
仲優樹 <yu-ki216m@outlook.jp>
2025-03-18 07:37:39 +0900

    Fixed screen orientation in portrait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'core/navigation/bottom_nav_bar.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
