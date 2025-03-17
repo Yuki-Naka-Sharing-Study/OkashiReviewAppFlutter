4513862606f27228c7051afd49e78c0573f80ebb
仲優樹 <yu-ki216m@outlook.jp>
2025-03-18 08:24:07 +0900

    Create a temporary review confirmation screen.

--- lib/features/check/presentation/check_screen.dart
+++ lib/features/check/presentation/check_screen.dart
@@ -8,17 +8,71 @@ class CheckScreen extends StatefulWidget {
 class _CheckScreenState extends State<CheckScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("レビュー確認"),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text("レビュータイトル"),
-            subtitle: Text("レビュー内容"),
-          )
-        ],
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("レビュー確認"),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: "チョコ"),
+              Tab(text: "キャラメル"),
+              Tab(text: "抹茶"),
+              Tab(text: "フルーツ"),
+              Tab(text: "その他"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+
+            ListView(
+              children: List.generate(10, (index) {
+                return ListTile(
+                  title: Text("チョコ"),
+                  subtitle: Text("これはチョコです。"),
+                );
+              }),
+            ),
+
+            ListView(
+              children: List.generate(10, (index) {
+                return ListTile(
+                  title: Text("キャラメル"),
+                  subtitle: Text("これはキャラメルです。"),
+                );
+              }),
+            ),
+
+            ListView(
+              children: List.generate(10, (index) {
+                return ListTile(
+                  title: Text("抹茶"),
+                  subtitle: Text("これは抹茶です。"),
+                );
+              }),
+            ),
+
+            ListView(
+              children: List.generate(10, (index) {
+                return ListTile(
+                  title: Text("フルーツ"),
+                  subtitle: Text("これはフルーツです。"),
+                );
+              }),
+            ),
+
+            ListView(
+              children: List.generate(10, (index) {
+                return ListTile(
+                  title: Text("その他"),
+                  subtitle: Text("これはその他です。"),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
